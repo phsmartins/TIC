9c45e476fa71d805db5b2a7845f868fac1f7fc42
pedro-ifms <phsmartins2004@gmail.com>
2022-10-14 13:07:01 -0400

    scrool suave e btn voltar ao topo

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ window.addEventListener('scroll', onScroll);
 onScroll();
 function onScroll() {
     showNavOnScroll();
+    showBackToTopButtonOnScroll();
+
+    activateMenuAtCurrentSection(home);
+    activateMenuAtCurrentSection(metaverso);
+    activateMenuAtCurrentSection(midiasDigitais);
+    activateMenuAtCurrentSection(bioinformatica);
 }
 
 function showNavOnScroll() {
@@ -12,6 +18,35 @@ function showNavOnScroll() {
       autoNav.classList.remove('scroll');
     }
   }
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute('id');
+  const menuElement = document.querySelector(`#autoNav a[href*=${sectionId}]`);
+
+  menuElement.classList.remove('active');
+  if (sectionBoundaries) {
+    menuElement.classList.add('active');
+  }
+}
 
 
 
